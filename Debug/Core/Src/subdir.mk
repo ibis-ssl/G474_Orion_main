b721412ad5fd4eaabe1f2c390deddfde94515e12
KANAIHIROYUKI <b1503146@planet.kanazawa-it.ac.jp>
2024-10-02 20:38:05 +0900

    プロジェクトファイルの更新

--- Debug/Core/Src/subdir.mk
+++ Debug/Core/Src/subdir.mk
@@ -11,6 +11,7 @@ C_SRCS += \
 ../Core/Src/buzzer_control.c \
 ../Core/Src/can_ibis.c \
 ../Core/Src/dma.c \
+../Core/Src/error.c \
 ../Core/Src/fdcan.c \
 ../Core/Src/gpio.c \
 ../Core/Src/icm20602_spi.c \
@@ -38,6 +39,7 @@ OBJS += \
 ./Core/Src/buzzer_control.o \
 ./Core/Src/can_ibis.o \
 ./Core/Src/dma.o \
+./Core/Src/error.o \
 ./Core/Src/fdcan.o \
 ./Core/Src/gpio.o \
 ./Core/Src/icm20602_spi.o \
@@ -65,6 +67,7 @@ C_DEPS += \
 ./Core/Src/buzzer_control.d \
 ./Core/Src/can_ibis.d \
 ./Core/Src/dma.d \
+./Core/Src/error.d \
 ./Core/Src/fdcan.d \
 ./Core/Src/gpio.d \
 ./Core/Src/icm20602_spi.d \
@@ -93,7 +96,7 @@ Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
 clean: clean-Core-2f-Src
 
 clean-Core-2f-Src:
-	-$(RM) ./Core/Src/actuator.cyclo ./Core/Src/actuator.d ./Core/Src/actuator.o ./Core/Src/actuator.su ./Core/Src/adc.cyclo ./Core/Src/adc.d ./Core/Src/adc.o ./Core/Src/adc.su ./Core/Src/ai_comm.cyclo ./Core/Src/ai_comm.d ./Core/Src/ai_comm.o ./Core/Src/ai_comm.su ./Core/Src/buzzer_control.cyclo ./Core/Src/buzzer_control.d ./Core/Src/buzzer_control.o ./Core/Src/buzzer_control.su ./Core/Src/can_ibis.cyclo ./Core/Src/can_ibis.d ./Core/Src/can_ibis.o ./Core/Src/can_ibis.su ./Core/Src/dma.cyclo ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma.su ./Core/Src/fdcan.cyclo ./Core/Src/fdcan.d ./Core/Src/fdcan.o ./Core/Src/fdcan.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/icm20602_spi.cyclo ./Core/Src/icm20602_spi.d ./Core/Src/icm20602_spi.o ./Core/Src/icm20602_spi.su ./Core/Src/keeper.cyclo ./Core/Src/keeper.d ./Core/Src/keeper.o ./Core/Src/keeper.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/odom.cyclo ./Core/Src/odom.d ./Core/Src/odom.o ./Core/Src/odom.su ./Core/Src/omni_wheel.cyclo ./Core/Src/omni_wheel.d ./Core/Src/omni_wheel.o ./Core/Src/omni_wheel.su ./Core/Src/ring_buffer.cyclo ./Core/Src/ring_buffer.d ./Core/Src/ring_buffer.o ./Core/Src/ring_buffer.su ./Core/Src/robot_control.cyclo ./Core/Src/robot_control.d ./Core/Src/robot_control.o ./Core/Src/robot_control.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/state_func.cyclo ./Core/Src/state_func.d ./Core/Src/state_func.o ./Core/Src/state_func.su ./Core/Src/stm32g4xx_hal_msp.cyclo ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_it.cyclo ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/stop_state_control.cyclo ./Core/Src/stop_state_control.d ./Core/Src/stop_state_control.o ./Core/Src/stop_state_control.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/system_stm32g4xx.cyclo ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su ./Core/Src/util.cyclo ./Core/Src/util.d ./Core/Src/util.o ./Core/Src/util.su
+	-$(RM) ./Core/Src/actuator.cyclo ./Core/Src/actuator.d ./Core/Src/actuator.o ./Core/Src/actuator.su ./Core/Src/adc.cyclo ./Core/Src/adc.d ./Core/Src/adc.o ./Core/Src/adc.su ./Core/Src/ai_comm.cyclo ./Core/Src/ai_comm.d ./Core/Src/ai_comm.o ./Core/Src/ai_comm.su ./Core/Src/buzzer_control.cyclo ./Core/Src/buzzer_control.d ./Core/Src/buzzer_control.o ./Core/Src/buzzer_control.su ./Core/Src/can_ibis.cyclo ./Core/Src/can_ibis.d ./Core/Src/can_ibis.o ./Core/Src/can_ibis.su ./Core/Src/dma.cyclo ./Core/Src/dma.d ./Core/Src/dma.o ./Core/Src/dma.su ./Core/Src/error.cyclo ./Core/Src/error.d ./Core/Src/error.o ./Core/Src/error.su ./Core/Src/fdcan.cyclo ./Core/Src/fdcan.d ./Core/Src/fdcan.o ./Core/Src/fdcan.su ./Core/Src/gpio.cyclo ./Core/Src/gpio.d ./Core/Src/gpio.o ./Core/Src/gpio.su ./Core/Src/icm20602_spi.cyclo ./Core/Src/icm20602_spi.d ./Core/Src/icm20602_spi.o ./Core/Src/icm20602_spi.su ./Core/Src/keeper.cyclo ./Core/Src/keeper.d ./Core/Src/keeper.o ./Core/Src/keeper.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/odom.cyclo ./Core/Src/odom.d ./Core/Src/odom.o ./Core/Src/odom.su ./Core/Src/omni_wheel.cyclo ./Core/Src/omni_wheel.d ./Core/Src/omni_wheel.o ./Core/Src/omni_wheel.su ./Core/Src/ring_buffer.cyclo ./Core/Src/ring_buffer.d ./Core/Src/ring_buffer.o ./Core/Src/ring_buffer.su ./Core/Src/robot_control.cyclo ./Core/Src/robot_control.d ./Core/Src/robot_control.o ./Core/Src/robot_control.su ./Core/Src/spi.cyclo ./Core/Src/spi.d ./Core/Src/spi.o ./Core/Src/spi.su ./Core/Src/state_func.cyclo ./Core/Src/state_func.d ./Core/Src/state_func.o ./Core/Src/state_func.su ./Core/Src/stm32g4xx_hal_msp.cyclo ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_it.cyclo ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/stop_state_control.cyclo ./Core/Src/stop_state_control.d ./Core/Src/stop_state_control.o ./Core/Src/stop_state_control.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/system_stm32g4xx.cyclo ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su ./Core/Src/tim.cyclo ./Core/Src/tim.d ./Core/Src/tim.o ./Core/Src/tim.su ./Core/Src/usart.cyclo ./Core/Src/usart.d ./Core/Src/usart.o ./Core/Src/usart.su ./Core/Src/util.cyclo ./Core/Src/util.d ./Core/Src/util.o ./Core/Src/util.su
 
 .PHONY: clean-Core-2f-Src
 
