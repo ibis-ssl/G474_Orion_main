013897a648e34abf1bd38f365da2a585e0426951
ibis.ssl.kanai@gmail.com <ibis.ssl.kanai@gmail.com>
2026-04-06 21:34:56 +0900

    125Hzに設定

--- Core/Inc/management.h
+++ Core/Inc/management.h
@@ -72,7 +72,7 @@ enum {
 #define LOW_VOLTAGE_LIMIT (22.5)
 
 #define MAIN_LOOP_CYCLE (500)
-#define ROBOT_INFO_CYCLE (120)
+#define ROBOT_INFO_CYCLE (125)
 #define PRINT_LOOP_CYCLE (60)
 #define PRINT_TUI_CYCLE (10)
 
